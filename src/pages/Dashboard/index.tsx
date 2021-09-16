88ee4725db14da7ecb574adc8f58194e49d05bb8
wakenedo <a_nuner@outlook.com>
2021-09-16 11:04:15 -0300

    Styling Dashboard Page - part 2
    Styling done, installed 'React-icons'

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Title, Form} from './styles';
+import {Title, Form, Repos} from './styles';
+import {FiChevronRight} from 'react-icons/fi'
 //import Logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
@@ -11,6 +12,20 @@ export const Dashboard: React.FC = () => {
           <input placeholder='username/repository_name'/>
           <button type='submit'>Search</button>
       </Form>
+
+      <Repos>
+        <a href='/repositories'>
+            <img src='' alt='Repository' />
+            <div>
+                <strong>wakenedo/mini-curso-reactjs</strong>
+                <p>Repositorio do mini curso de React.js</p>
+            </div>
+            <FiChevronRight size={20}/>
+        </a>
+        {/*Instalamos o React icons, para utilizar diversas bibliotecas de icones 
+        com facilidade. material ui, fonts awasome, bootstrap, feather icons*/}
+
+      </Repos>
       </>
   )
 };
