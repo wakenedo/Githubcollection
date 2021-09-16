d0f70dcd125a20a5effe043a086ad3f538aaecfa
wakenedo <a_nuner@outlook.com>
2021-09-16 15:34:59 -0300

    Repository Details Page - Part 2
    Changed the static code into dynamic, linking the paths to the public Github api

--- src/pages/Dashboard/Repo/index.tsx
+++ src/pages/Dashboard/Repo/index.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
+import { api } from '../../../services/api';
 
 import { Header, RepoInfo, Issues } from './styles'
 
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
-
-
 interface RepositoryParams {
   repository: string;
 }
 
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+  owner: {
+      login: string;
+      avatar_url: string;
+  },
+}
+
+interface GithubIssue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 export const Repo: React.FC = () => {
+  const [repository, setRepository] = 
+  React.useState<GithubRepository | null>(null)
+  const [issues, setIssues] = React.useState<GithubIssue[]>([])
   const {params} = useRouteMatch<RepositoryParams>();
+
+  React.useEffect(() => {
+    api
+    .get(`repos/${params.repository}`)
+    .then(response => setRepository(response.data));
+    api
+    .get(`repos/${params.repository}/issues`)
+    .then(response => setIssues(response.data));
+
+
+  }, [params.repository]);
   return  (
   <>
   <Header>
@@ -20,39 +54,48 @@ export const Repo: React.FC = () => {
     <Link to='/'>Voltar</Link> 
   </Header>
 
-  <RepoInfo>
+  {repository && (
+    <RepoInfo>
     <header>
-      <img src='' alt='Alexandre Developer' />
+      <img 
+        src={repository.owner.avatar_url} 
+        alt={repository.owner.login} 
+      />
       <div>
-        <strong>wakenedo/mini-curso-reactjs</strong>
-        <p>Repositorio do mini-curos de reactjs</p>
+        <strong>{repository.full_name}</strong>
+        <p>{repository.description}</p>
       </div>
     </header>
     <ul>
       <li>
-        <strong>2330</strong>
+        <strong>{repository.stargazers_count}</strong>
         <span>Stars</span>
       </li>
       <li>
-        <strong>210</strong>
+        <strong>{repository.forks_count}</strong>
         <span>Forks</span>
       </li>
       <li>
-        <strong>79</strong>
+        <strong>{repository.open_issues_count}</strong>
         <span>Issues abertas</span>
       </li>      
     </ul>
   </RepoInfo>
+  )}
+
+  
 
   <Issues>
-    <Link to='/'>
+    {issues.map(issues =>((
+      <a href={issues.html_url} key={issues.id}>
       <div>
-        <strong>sdadasdasdasdasda</strong>
-        <p>sadasdasdasdasdads</p>
+        <strong>{issues.title}</strong>
+        <p>{issues.user.login}</p>
       </div>
 
       <FiChevronRight size={20}/>
-    </Link>
+    </a>
+  )))}
   </Issues>
   </>
   );
