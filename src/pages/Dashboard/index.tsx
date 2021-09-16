1a1d9ad98c068ffcc5bb46b6f6942750511f91e6
wakenedo <a_nuner@outlook.com>
2021-09-16 12:11:17 -0300

    Integrating Github api
    Fixed the static elements to dynamic. installed 'Axios'

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,29 +1,64 @@
 import React from 'react';
+import {api} from '../../services/api'
 import {Title, Form, Repos} from './styles';
 import {FiChevronRight} from 'react-icons/fi'
-//import Logo from '../../assets/logo.svg';
 
+
+interface GithubRepository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    },
+}
 export const Dashboard: React.FC = () => {
+  const [repos, setRepos] = React.useState<GithubRepository[]>([]);  
+  const [newRepo, setNewRepo] = React.useState('');
+  
+  function handleInputChange(event:React.ChangeEvent<HTMLInputElement>) : void {
+      setNewRepo(event.target.value);
+  }
+  
+  async function handleAddRepo(
+      event:React.FormEvent<HTMLFormElement>
+   ): Promise<void> {
+      event.preventDefault();
+      const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+
+      console.log(response)
+
+      const repository = response.data;
+      
+      
+      setRepos([...repos, repository]);
+      setNewRepo('')
+  }
+  
   return  (
       <>
       {/*<img src={Logo} alt='GitCollection'/>*/}
       <Title>Github Catalog</Title>
-      <Form>
-          <input placeholder='username/repository_name'/>
+      <Form onSubmit={handleAddRepo}>
+          <input placeholder='username/repository_name' 
+          onChange={handleInputChange}/>
           <button type='submit'>Search</button>
       </Form>
 
       <Repos>
-        <a href='/repositories'>
-            <img src='' alt='Repository' />
+        {repos.map(repository => (
+            <a href='/repositories' key={repository.full_name}>
+            <img 
+                src={repository.owner.avatar_url} 
+                alt={repository.owner.login}
+            />
             <div>
-                <strong>wakenedo/mini-curso-reactjs</strong>
-                <p>Repositorio do mini curso de React.js</p>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20}/>
         </a>
-        {/*Instalamos o React icons, para utilizar diversas bibliotecas de icones 
-        com facilidade. material ui, fonts awasome, bootstrap, feather icons*/}
+        ))}
 
       </Repos>
       </>
